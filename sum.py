d929f3e222dabcf3521640a2456b8831cf675351
Bhuvanaa-S <bhuvanaaa2006@gmail.com>
2025-04-23 13:48:33 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 def add_first_n_numbers(n):
     return n * (n + 1) // 2
 
-n = 5  # Predefined value
+n = 6 # Predefined value
 result = add_first_n_numbers(n)
 print(f"The sum of the first {n} natural numbers is: {result}")
